c863a93996029b6546153d180f4d606a4e452e7a
Karl Runge <karl.spartacus@gmail.com>
2021-10-06 00:33:17 +1100

    Added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { GlobalStyle } from './styles/GlobalStyle'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import NewUserInput from './components/NewUserInput'
 import AvailableUsers from './components/AvailableUsers'
