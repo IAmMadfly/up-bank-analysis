ebf8569323944addafd1d69fd903df6756781aea
Karl Runge <karl.spartacus@gmail.com>
2021-10-07 00:48:32 +1100

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 import { GlobalStyle } from './styles/GlobalStyle'
+import { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import NewUserInput from './components/NewUserInput'
 import AvailableUsers from './components/AvailableUsers'
 
 export function App() {
+
+  const [userId, setUserId] = useState(null)
+
+  const NewUser = () => {
+    return (
+      <div>
+        <GlobalStyle />
+        <NewUserInput />
+        <AvailableUsers />
+      </div>
+    )
+  }
+
+  const UserInformation = () => {
+    return (
+      <div>
+        <p>User information</p>
+      </div>
+    )
+  }
+
+  console.log("URL:", window.location)
+
   return (
     <>
-      <GlobalStyle />
-      <NewUserInput />
-      <AvailableUsers />
+      <Router>
+        <Route exact path="/main_window" >
+          <NewUser/>
+        </Route>
+        {/* <Route exact path="/userinfo">
+          <UserInformation/>
+        </Route> */}
+      </Router>
     </>
   )
 }
